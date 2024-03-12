d6eb36d720349d039e16013879e6d572c0d4b09e
chajuneseok <wnstjr401@naver.com>
2024-03-12 12:55:08 +0900

    feat : add page component Architecture

---
+++ yeogiro/src/pages/botton sheet/SafetyGuideline/SafetyGuidelineMain.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Map = () => {
+  return (
+    <View>
+      <Text style={styles.text}>SafetyGuidelineMain</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+});
+
+export default Map;
