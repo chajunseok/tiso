6d62856f30ffe2334e1b2b7a892688b98115b24b
kang-dan <eodms4334@naver.com>
2024-03-21 14:31:54 +0900

    feat : 행동요령 폴더 수정 및 기능 추가
    Change-Id: I4dd815c37dd924208f290818b63af16a47d9f5b8

---
+++ yeogiro/src/pages/Bottomsheet/SafetyGuideline/SafetyCategory/NaturalDisaster.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import {View, Text, StyleSheet, Image, FlatList} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+
+const disasterList = [
+  {id: '1', title: '대설'},
+  {id: '2', title: '홍수'},
+  {id: '3', title: '침수'},
+  {id: '4', title: '해일'},
+  {id: '5', title: '폭염'},
+  {id: '6', title: '지진'},
+  {id: '7', title: '한파'},
+  {id: '8', title: '건조'},
+  {id: '9', title: '호우'},
+  {id: '10', title: '태풍'},
+];
+
+function NaturalDisaster({navigation}) {
+  const renderItem = ({item}) => (
+    <TouchableOpacity
+      style={styles.item}
+      onPress={() =>
+        navigation.navigate('SafetyGuidelineDetail', {title: item.title})
+      }>
+      <Text style={styles.title}>{item.title}</Text>
+      <Image
+        source={require('../../../../../assets/icons/Next.png')}
+        style={styles.icon}
+      />
+    </TouchableOpacity>
+  );
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={disasterList}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        style={styles.flatList}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+  flatList: {
+    padding: 10,
+  },
+  item: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginBottom: 10,
+    backgroundColor: '#f0f0f0',
+    borderRadius: 8,
+  },
+  title: {
+    fontSize: 18,
+    color: '#333333',
+    fontWeight: 'bold',
+  },
+  icon: {
+    width: 20,
+    height: 20,
+    resizeMode: 'contain',
+  },
+});
+
+export default NaturalDisaster;
--- yeogiro/src/pages/Bottomsheet/SafetyGuideline/SafetyGuidelineDetail.jsx
+++ yeogiro/src/pages/Bottomsheet/SafetyGuideline/SafetyGuidelineDetail.jsx
@@ -1,10 +1,16 @@
+// SafetyGuidelineDetail.jsx
+
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
-const SafetyGuidelineDetail = () => {
+const SafetyGuidelineDetail = ({route}) => {
+  // route.params에 전달된 title이 있을 경우 가져와서 사용합니다.
+  const {title} = route.params || {};
+
   return (
-    <View>
-      <Text style={styles.text}>SafetyGuidelineDetail</Text>
+    <View style={styles.container}>
+      {/* 가져온 title을 보여줍니다. */}
+      <Text style={styles.text}>{title}</Text>
     </View>
   );
 };
