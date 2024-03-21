b924c905c281e206a45c0bf2882def446b990749
ureonmyhitlist <ureonmyhitlist@naver.com>
2024-03-21 10:52:43 +0900

    refactor : 시설 상세 정보 UI 수정
    Change-Id: I5ec42495d1569336b147e0865f29c0894e2249c7

--- yeogiro/src/pages/Bottomsheet/FacilitiesInfo/PharmacyInfoDetail.jsx
+++ yeogiro/src/pages/Bottomsheet/FacilitiesInfo/PharmacyInfoDetail.jsx
@@ -1,10 +1,105 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {FlatList} from 'react-native-gesture-handler';
 
-const PharmacyInfoDetail = () => {
+const HospitalInfoDetail = () => {
+  const DATA = [
+    {
+      id: '1',
+      title: '약국 1',
+      address: '대전 서구 청사로 128 칼릭스 빌딩 5-8층 (월평동)',
+      telephone: '042-123-4567',
+    },
+    {
+      id: '2',
+      title: '약국 2',
+      address: '대전 서구 청사로 128 칼릭스 빌딩 5-8층 (월평동)',
+      telephone: '042-123-4567',
+    },
+    {
+      id: '3',
+      title: '약국 3',
+      address: '대전 서구 청사로 128 칼릭스 빌딩 5-8층 (월평동)',
+      telephone: '042-123-4567',
+    },
+    {
+      id: '4',
+      title: '약국 4',
+      address: '대전 서구 청사로 128 칼릭스 빌딩 5-8층 (월평동)',
+      telephone: '042-123-4567',
+    },
+    {
+      id: '5',
+      title: '약국 5',
+      address: '대전 서구 청사로 128 칼릭스 빌딩 5-8층 (월평동)',
+      telephone: '042-123-4567',
+    },
+    {
+      id: '6',
+      title: '약국 6',
+      address: '대전 서구 청사로 128 칼릭스 빌딩 5-8층 (월평동)',
+      telephone: '042-123-4567',
+    },
+    {
+      id: '7',
+      title: '약국 7',
+      address: '대전 서구 청사로 128 칼릭스 빌딩 5-8층 (월평동)',
+      telephone: '042-123-4567',
+    },
+    {
+      id: '8',
+      title: '약국 8',
+      address: '대전 서구 청사로 128 칼릭스 빌딩 5-8층 (월평동)',
+      telephone: '042-123-4567',
+    },
+    {
+      id: '9',
+      title: '약국 9',
+      address: '대전 서구 청사로 128 칼릭스 빌딩 5-8층 (월평동)',
+      telephone: '042-123-4567',
+    },
+    {
+      id: '10',
+      title: '약국 10',
+      address: '대전 서구 청사로 128 칼릭스 빌딩 5-8층 (월평동)',
+      telephone: '042-123-4567',
+    },
+    {
+      id: '11',
+      title: '약국 11',
+      address: '대전 서구 청사로 128 칼릭스 빌딩 5-8층 (월평동)',
+      telephone: '042-123-4567',
+    },
+    {
+      id: '12',
+      title: '약국 12',
+      address: '대전 서구 청사로 128 칼릭스 빌딩 5-8층 (월평동)',
+      telephone: '042-123-4567',
+    },
+  ];
+
+  const renderItem = ({item}) => {
+    return (
+      <View style={styles.pharmacy}>
+        <Text style={{color: 'black', fontSize: 18, fontWeight: 600}}>
+          {item.title}
+        </Text>
+        <Text>{item.address}</Text>
+        <Text style={{color: 'blue'}}>{item.telephone}</Text>
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Text style={styles.text}>약국 정보</Text>
+    <View style={styles.container}>
+      <View style={{borderBottomWidth: 1, borderBottomColor: '#CED4DA'}}>
+        <Text style={styles.containerTitle}>약국 정보</Text>
+      </View>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
     </View>
   );
 };
@@ -16,11 +111,19 @@ const styles = StyleSheet.create({
     borderTopWidth: 1,
     borderTopColor: '#CED4DA',
   },
-  text: {
+  pharmacy: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#CED4DA',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+  },
+  containerTitle: {
     fontSize: 20,
     fontWeight: 'bold',
     color: 'black',
+    marginHorizontal: 20,
+    marginVertical: 25,
   },
 });
 
-export default PharmacyInfoDetail;
+export default HospitalInfoDetail;
