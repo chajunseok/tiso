46109d3cb4caa489a1f18bd8b64f241a6614660f
chajuneseok <wnstjr401@naver.com>
2024-03-22 13:59:47 +0900

    feat : 수신 유형 설정, 수신 지역 설정
    Change-Id: I70932216ba01cd6c7467fa295d9e3386b62ecfe6

--- yeogiro/src/pages/Bottomsheet/SystemSetting/MessageTypeSettings/MessageTypeSettings.jsx
+++ yeogiro/src/pages/Bottomsheet/SystemSetting/MessageTypeSettings/MessageTypeSettings.jsx
@@ -1,25 +1,101 @@
-import React from 'react';
+import React, {useState, useLayoutEffect} from 'react';
+import {Switch} from 'react-native';
 import {View, Text, StyleSheet} from 'react-native';
 
-const Map = () => {
+function SettingsScreen({navigation}) {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: '환경 설정',
+      headerTitleStyle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 5,
+      },
+      headerTitleAlign: 'center',
+    });
+  }, [navigation]);
+
+  const [isEmergencyEnabled, setIsEmergencyEnabled] = useState(false);
+  const [isSafetyEnabled, setIsSafetyEnabled] = useState(false);
+
+  const toggleEmergencySwitch = () =>
+    setIsEmergencyEnabled(previousState => !previousState);
+  const toggleSafetySwitch = () =>
+    setIsSafetyEnabled(previousState => !previousState);
+
   return (
-    <View>
-      <Text style={styles.text}>SystemSettingMain</Text>
+    <View style={styles.container}>
+      <View style={styles.contentbox}>
+        <Text style={styles.title}>수신 유형 설정</Text>
+        <Text style={styles.subtitle}>수신 유형을 변경합니다</Text>
+      </View>
+      <View style={styles.content}>
+        <View style={styles.contentbox}>
+          <Text style={styles.title}>긴급 재난 문자 설정</Text>
+        </View>
+        <View style={styles.switchContainer}>
+          <Switch
+            trackColor={{false: '#767577', true: '#81b0ff'}}
+            thumbColor={isEmergencyEnabled ? '767577' : '#f4f3f4'}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={toggleEmergencySwitch}
+            value={isEmergencyEnabled}
+            style={styles.switchStyle}
+          />
+        </View>
+      </View>
+      <View style={styles.content}>
+        <View style={styles.contentbox}>
+          <Text style={styles.title}>안전 안내 문자 설정</Text>
+        </View>
+        <View style={styles.switchContainer}>
+          <Switch
+            trackColor={{false: '#767577', true: '#81b0ff'}}
+            thumbColor={isSafetyEnabled ? '767577' : '#f4f3f4'}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={toggleSafetySwitch}
+            value={isSafetyEnabled}
+            style={styles.switchStyle}
+          />
+        </View>
+      </View>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
+  switchContainer: {
+    marginTop: 10,
+  },
+  switchStyle: {
+    transform: [{scaleX: 1.5}, {scaleY: 1.5}],
+    marginRight: 5,
+  },
+
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    padding: 10,
+    backgroundColor: '#ffff',
   },
-  text: {
+  content: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 5,
+  },
+  title: {
     fontSize: 20,
+    color: '#262626',
     fontWeight: 'bold',
-    color: 'black',
+  },
+  subtitle: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  contentbox: {
+    marginBottom: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#eaeaea',
   },
 });
 
-export default Map;
+export default SettingsScreen;
--- yeogiro/src/pages/Bottomsheet/SystemSetting/RegionalSettings/RegionalSettings.jsx
+++ yeogiro/src/pages/Bottomsheet/SystemSetting/RegionalSettings/RegionalSettings.jsx
@@ -1,17 +1,104 @@
-// OtherScreen.js
-import React from 'react';
-import {View, Button} from 'react-native';
+import React, {useLayoutEffect} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {useCities} from '../../CitiesContext';
 
 function RegionalSetting({navigation}) {
+  const {addedCities, removeCity} = useCities();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: '환경 설정',
+      headerTitleStyle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 5,
+      },
+      headerTitleAlign: 'center',
+    });
+  }, [navigation]);
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button title="Modal" onPress={() => navigation.navigate('Modal')} />
-      <Button
-        title="RegionalAdd"
-        onPress={() => navigation.navigate('RegionalAdd')}
-      />
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <View style={styles.contentbox}>
+          <Text style={styles.title}>수신 지역 설정</Text>
+          <Text style={styles.subtitle}>원하는 지역을 추가합니다</Text>
+        </View>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('RegionalAdd')}>
+          <Text style={styles.buttonText}>지역 추가</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.cityList}>
+        {addedCities.map((city, index) => (
+          <View key={index} style={styles.cityItem}>
+            <Text style={styles.cityName}>{city}</Text>
+            <TouchableOpacity
+              style={styles.removeButton}
+              onPress={() => removeCity(city)}>
+              <Text style={styles.removeButtonText}>삭제</Text>
+            </TouchableOpacity>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: '#ffff',
+  },
+  content: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    color: '#262626',
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  button: {
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    backgroundColor: 'rgb(178, 201, 219)',
+    borderRadius: 5,
+    marginTop: -10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 10,
+    fontWeight: 'bold',
+  },
+  cityName: {
+    fontSize: 18,
+    color: '#262626',
+    paddingVertical: 5,
+  },
+  cityItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 5,
+  },
+  removeButton: {
+    marginLeft: 10,
+    backgroundColor: 'red',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 3,
+  },
+  removeButtonText: {
+    color: '#ffffff',
+    fontSize: 10,
+  },
+});
 
 export default RegionalSetting;
