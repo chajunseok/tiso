23ebea2a92e322ddbb847e14a2aa31cdeaa9c61d
tjwjdgus83 <tjwjdgus83@naver.com>
2024-03-25 16:18:42 +0900

    feat : 대피소 api 초기형태 추가 및 응답스키마 적용
    Change-Id: I8e73465112521532c9150969daab08a086c14677

--- BackEnd/api/shelter.py
+++ BackEnd/api/shelter.py
@@ -1,4 +1,14 @@
 from fastapi import APIRouter
 
+from schema.response import ShelterPathSchema
+
 
 router=APIRouter(prefix="/shelters",tags=["shelters"])
+
+
+@router.get("/{shelter_id}",status_code=200)
+def get_path_handler(shelter_id:str,latitude:float,longitude:float)->ShelterPathSchema:
+    print({shelter_id})
+    print({latitude})
+    print({longitude})
+    return ShelterPathSchema(path=[[1,2],[3,4]],distance=12)
