7feabfdb59a3f630f69508a246e845dd46d109af
tjwjdgus83 <tjwjdgus83@naver.com>
2024-03-25 10:39:44 +0900

    feat : 필요한 폴더 및 자료구조 생성 및 로딩
    Change-Id: Ic31d877788dee927adb95e2775126981afcffa64

--- data_processing/process_map.py
+++ data_processing/process_map.py
@@ -72,3 +72,78 @@ def make_short_path(binary_map,gps_mapping_list,walkable_spot_np_list,shelter_id
     with open(f"{path_dir}/{shelter_id}.pkl", 'wb') as f:
         pickle.dump(result, f)
 
+def make_short_path_wrapper(args):
+    # make_short_path 함수를 호출하는 래퍼 함수
+    # args는 (shelter_id, shelter_info, binary_map, gps_mapping_list, walkable_spot_np_list)의 튜플이다.
+    binary_map, gps_mapping_list, walkable_spot_np_list, shelter_id, shelter_info = args
+    make_short_path(binary_map, gps_mapping_list, walkable_spot_np_list, shelter_id, shelter_info)
+
+def chunks(lst, n):
+    # 리스트를 n 크기의 청크로 나누는 유틸리티 함수
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+if __name__ == "__main__":
+    # preparing folder
+    createFolder([binary_dir, path_dir,walkable_spot_dir,union_binary_map_dir])
+    image_list = os.listdir(image_dir)
+    binary_image_list = os.listdir(binary_dir)
+
+    image_png_list=[file for file in image_list if file.endswith(".png")]
+    
+    for file_name in image_png_list:
+        print(
+            f"=======================make_binary_txt_from_image {file_name}==============================="
+        )
+        binary_name = os.path.splitext(file_name)[0] + ".txt"
+        if binary_name not in binary_image_list:
+            make_binary_txt_from_image(file_name, image_dir, binary_name, binary_dir)
+        print(
+            f"=======================make_binary_txt_from_image {file_name} done==============================="
+        )
+
+    #map들에 대한 이진맵이 없다면 이진맵을 준비합니다.
+    if not os.path.isfile(f"{union_binary_map_dir}/union_binary_map.txt"):
+        splited_file_name=list(map(lambda file_name:list(map(int,pathlib.Path(file_name).stem.split("_"))),image_png_list))
+
+        splited_file_name.sort(key=lambda x:(x[1],x[0]))
+
+        splited_file_name=list(map(lambda x:str(x[0])+"_"+str(x[1])+".txt",splited_file_name))
+
+        reshaped_file_name_list=np.array(splited_file_name).reshape(33,35)    
+
+        union_binary_map=np.array([])
+
+        for line_numpy_binary_map in reshaped_file_name_list:
+            line_binary_map_stack=np.array([])
+
+            for file_name in line_numpy_binary_map:
+                binary_map = np.loadtxt(
+                    f"{binary_dir}/{file_name}", delimiter=" ", dtype="int"
+                )   
+                if line_binary_map_stack.size==0:
+                    line_binary_map_stack=binary_map
+                else:
+                    line_binary_map_stack=np.concatenate([line_binary_map_stack,binary_map],axis=1)
+            if union_binary_map.size==0:
+                union_binary_map=line_binary_map_stack
+            else:
+                union_binary_map=np.concatenate((union_binary_map,line_binary_map_stack),axis=0)
+        np.savetxt(f"{union_binary_map_dir}/union_binary_map.txt", union_binary_map, fmt='%d', delimiter=' ')
+        print("============saved union binary map===========")
+
+    binary_map=get_binary_map()
+    walkable_spot_tuple = np.where(binary_map == 1)
+    walkable_spot_np_list=np.column_stack((walkable_spot_tuple[0],walkable_spot_tuple[1]))
+
+    #갈수 있는 모든 지점들을 준비합니다.
+    if not os.path.isfile(f"{walkable_spot_dir}/walable_spot.txt"):
+        np.savetxt(f"{walkable_spot_dir}/walable_spot.txt", walkable_spot_np_list, fmt='%d', delimiter=' ')
+
+    print(f"{walkable_spot_np_list.shape[0]} walkable spot was detected")
+
+    gps_mapping_list=get_coordinate_gps_mapping()
+    shelter_dict=get_shelter_index()
+
+
+    
