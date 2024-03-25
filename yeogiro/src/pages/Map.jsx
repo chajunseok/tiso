5a3edfaf2b0ca51d1285725b34c4bce079a7bd12
ureonmyhitlist <ureonmyhitlist@naver.com>
2024-03-25 13:40:54 +0900

    feat : 현재 위치 정보 권한 얻기 구현
    Change-Id: I729b9ed8858a2b7937c61a3d2dd9473632c6dc61

--- yeogiro/src/pages/Map.jsx
+++ yeogiro/src/pages/Map.jsx
@@ -1,24 +1,111 @@
-import NaverMapView, {Circle, Marker, Path, Polyline, Polygon} from "react-native-nmap";
+import React, {useEffect, useState} from 'react';
+import { PermissionsAndroid, Platform } from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
+import NaverMapView, {
+  Circle,
+  Marker,
+  Path,
+  Polyline,
+  Polygon,
+} from 'react-native-nmap';
 
 function MyMap() {
-    const P0 = {latitude: 37.564362, longitude: 126.977011};
-    const P1 = {latitude: 37.565051, longitude: 126.978567};
-    const P2 = {latitude: 37.565383, longitude: 126.976292};
+  const [currentLocation, setCurrentLocation] = useState(null);
 
-    return <NaverMapView style={{width: '100%', height: '100%'}}
-                         showsMyLocationButton={true}
-                         center={{...P0, zoom: 16}}
-                         onTouch={e => console.warn('onTouch', JSON.stringify(e.nativeEvent))}
-                         onCameraChange={e => console.warn('onCameraChange', JSON.stringify(e))}
-                         onMapClick={e => console.warn('onMapClick', JSON.stringify(e))}>
-        <Marker coordinate={P0} onClick={() => console.warn('onClick! p0')}/>
-        <Marker coordinate={P1} pinColor="blue" onClick={() => console.warn('onClick! p1')}/>
-        <Marker coordinate={P2} pinColor="red" onClick={() => console.warn('onClick! p2')}/>
-        <Path coordinates={[P0, P1]} onClick={() => console.warn('onClick! path')} width={10}/>
-        <Polyline coordinates={[P1, P2]} onClick={() => console.warn('onClick! polyline')}/>
-        <Circle coordinate={P0} color={"rgba(255,0,0,0.3)"} radius={200} onClick={() => console.warn('onClick! circle')}/>
-        <Polygon coordinates={[P0, P1, P2]} color={`rgba(0, 0, 0, 0.5)`} onClick={() => console.warn('onClick! polygon')}/>
+  useEffect(() => {
+    // 위치 권한 요청
+    const requestLocationPermission = async () => {
+      try {
+        if (Platform.OS === 'android') {
+          const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            {
+              title: 'Location Permission',
+              message: 'This app needs access to your location.',
+              buttonNeutral: 'Ask Me Later',
+              buttonNegative: 'Cancel',
+              buttonPositive: 'OK',
+            },
+          );
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            // 위치 권한 허용됨
+            getCurrentLocation();
+          } else {
+            console.log('Location permission denied');
+          }
+        } else {
+          // Android 이외의 플랫폼에서는 바로 위치를 가져옴
+          getCurrentLocation();
+        }
+      } catch (err) {
+        console.warn(err);
+      }
+    };
+
+    requestLocationPermission();
+  }, []);
+
+  const getCurrentLocation = () => {
+    Geolocation.getCurrentPosition(
+      position => {
+        const {latitude, longitude} = position.coords;
+        setCurrentLocation({latitude, longitude});
+      },
+      error => {
+        console.warn(error.message);
+      },
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
+    );
+  };
+  const P0 = {latitude: 37.564362, longitude: 126.977011};
+  const P1 = {latitude: 37.565051, longitude: 126.978567};
+  const P2 = {latitude: 37.565383, longitude: 126.976292};
+
+  return (
+    <NaverMapView
+      style={{width: '100%', height: '100%'}}
+      showsMyLocationButton={true}
+      center={
+        currentLocation
+          ? {...currentLocation, zoom: 16}
+          : {latitude: 37.564362, longitude: 126.977011, zoom: 16}
+      }
+      onTouch={e => console.warn('onTouch', JSON.stringify(e.nativeEvent))}
+      onCameraChange={e => console.warn('onCameraChange', JSON.stringify(e))}
+      onMapClick={e => console.warn('onMapClick', JSON.stringify(e))}>
+      <Marker coordinate={P0} onClick={() => console.warn('onClick! p0')} />
+      <Marker
+        coordinate={P1}
+        pinColor="blue"
+        onClick={() => console.warn('onClick! p1')}
+      />
+      <Marker
+        coordinate={P2}
+        pinColor="red"
+        onClick={() => console.warn('onClick! p2')}
+      />
+      <Path
+        coordinates={[P0, P1]}
+        onClick={() => console.warn('onClick! path')}
+        width={10}
+      />
+      <Polyline
+        coordinates={[P1, P2]}
+        onClick={() => console.warn('onClick! polyline')}
+      />
+      <Circle
+        coordinate={P0}
+        color={'rgba(255,0,0,0.3)'}
+        radius={200}
+        onClick={() => console.warn('onClick! circle')}
+      />
+      <Polygon
+        coordinates={[P0, P1, P2]}
+        color={`rgba(0, 0, 0, 0.5)`}
+        onClick={() => console.warn('onClick! polygon')}
+      />
     </NaverMapView>
+  );
 }
 
 export default MyMap;
