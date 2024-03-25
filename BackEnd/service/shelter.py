463a9f306dab646efe30595df6d441b2596dfb75
tjwjdgus83 <tjwjdgus83@naver.com>
2024-03-25 21:27:39 +0900

    feat : ShelterService의 레포지토리 의존성 주입
    Change-Id: I5d8104b1d2e9a34ed7f3d1c758328f6c6684c327

--- BackEnd/service/shelter.py
+++ BackEnd/service/shelter.py
@@ -1,3 +1,7 @@
+from fastapi import Depends
+from repository.shelter import ShelterRepository
+
 class ShelterService:
-    def __init__(self):
-        print("ShelterService inti")
+    def __init__(self,shelter_repository:ShelterRepository = Depends()):
+        self.shelter_repository=shelter_repository
+        print("ShelterService init")
