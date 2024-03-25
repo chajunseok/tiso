6cc726a866f17dacfe5a78035a62c98559cb8a05
tjwjdgus83 <tjwjdgus83@naver.com>
2024-03-25 16:56:03 +0900

    edit : 핑퐁 응답스키마 기본값 추가 및 변경
    Change-Id: I1871ec653b81847f436ae8e9ddc9fc1c1f078700

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+from schema.response import PingPongSchema
 from api import shelter
 
 app = FastAPI()
 app.include_router(shelter.router)
 
 @app.get("/",status_code=200)
-async def root():
+async def root() -> PingPongSchema:
     return {"message": "Hello World"}
--- BackEnd/schema/response.py
+++ BackEnd/schema/response.py
@@ -1,5 +1,8 @@
 from pydantic import BaseModel
 
+class PingPongSchema(BaseModel):
+    message : str ="Hello World"
+
 class ShelterPathSchema(BaseModel):
     path: list[tuple[float,float]] = [(37.123,127.233),(37.55,127.6466)]
     distance: float = 1001.53125
