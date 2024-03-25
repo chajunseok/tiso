52d4b626a457663d10f96f4b66e359c2d6ebaad8
tjwjdgus83 <tjwjdgus83@naver.com>
2024-03-25 21:28:39 +0900

    feat : ShelterRepository의 mongo client 의존성 주입
    Change-Id: I77578cfb757a7b571c6986db0109fc506e0f1419

--- BackEnd/repository/shelter.py
+++ BackEnd/repository/shelter.py
@@ -1,4 +1,8 @@
+from fastapi import Depends
+from pymongo import MongoClient
+from database.mongo import get_client
+
 class ShelterRepository:
-    def __init__(self,connection):
-        self.connection=connection
-        print("ShelterRepository inti")
+    def __init__(self,client:MongoClient=Depends(get_client)):
+        self.client=client
+        print("ShelterRepository init")
