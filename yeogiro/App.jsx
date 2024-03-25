22445dcac44f5339cc2f00decafec7774077e96e
chajunseok <wnstjr401@gmail.com>
2024-03-25 14:40:18 +0900

    refactor : 푸시 기능 추가
    Change-Id: Ie4a6334cacd0d4c9ff653213aa95c9182711759c

--- yeogiro/App.jsx
+++ yeogiro/App.jsx
@@ -1,19 +1,41 @@
 import React, {useEffect} from 'react';
 import {Alert} from 'react-native';
-import Map from './src/pages/Map';
-import BottomSheet from './src/pages/BottomSheet';
+import messaging from '@react-native-firebase/messaging';
+import PushNotification from 'react-native-push-notification';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import BottomSheet from './src/pages/BottomSheet';
+import Map from './src/pages/Map';
 
-const Stack = createStackNavigator();
-
-function App() {
+const App = () => {
   useEffect(() => {
-    const unsubscribe = messaging().onMessage(async remoteMessage => {
-      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
-    });
+    // 사용자로부터 알림 권한 요청
+    const requestUserPermission = async () => {
+      const authStatus = await messaging().requestPermission();
+      const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
 
+      if (enabled) {
+        console.log('Authorization status:', authStatus);
+      }
+    };
+
+    requestUserPermission();
+
+    // 포그라운드 메시지 리스너 등록
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      const {title, body} = remoteMessage.notification;
+      const imageUrl = remoteMessage.data.imageUrl;
+
+      PushNotification.localNotification({
+        title: title,
+        message: body,
+        largeIconUrl: imageUrl, // 이미지 URL
+        smallIcon: 'ic_notification', // 앱의 작은 아이콘
+        bigPictureUrl: imageUrl,
+      });
+    });
+    // Clean up the onMessage listener
     return unsubscribe;
   }, []);
 
@@ -29,6 +51,6 @@ function App() {
       <BottomSheet />
     </GestureHandlerRootView>
   );
-}
+};
 
 export default App;
