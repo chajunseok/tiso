89c80088f0d3fb3eab26749cad83de3fa452946c
tjwjdgus83 <tjwjdgus83@naver.com>
2024-03-26 09:50:42 +0900

    feat : 안전한 몽고 DB 해제를 위한 생명주기 적용
    Change-Id: I8f0d54b8047760d6a72998db3d3d5d548eedc3e9

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,8 +1,16 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from database.mongo import mongodb
 from schema.response import PingPongSchema
 from api import shelter
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    mongodb.connect()
+    yield
+    mongodb.close()
+    
+app = FastAPI(lifespan=lifespan)
 app.include_router(shelter.router)
 
 @app.get("/",status_code=200)
