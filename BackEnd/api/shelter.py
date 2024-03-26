009b1e5bffec02d2ff215f0f57883e07ce9b27a7
tjwjdgus83 <tjwjdgus83@naver.com>
2024-03-26 09:56:37 +0900

    edit : api 비동기로 변경
    Change-Id: I0d8671fe4de64399ed339374b188bea14503f361

--- BackEnd/api/shelter.py
+++ BackEnd/api/shelter.py
@@ -8,7 +8,7 @@ router=APIRouter(prefix="/shelters",tags=["shelters"])
 
 
 @router.get("/{shelter_id}",status_code=200)
-def get_path_handler(
+async def get_path_handler(
         shelter_id:str,
         latitude:float,
         longitude:float,
