a0060f99f9032e1f704dd22738a373a520e6dd22
tjwjdgus83 <tjwjdgus83@naver.com>
2024-03-26 21:04:39 +0900

    feat : shelter_service 함수 타입힌트 명시
    Change-Id: Id1fd51e5408833c33ad64e3955c5779e747afee0
    
    Former-commit-id: 1d426c362f2f2b13ca972fb779cfd3fef832f469

--- BackEnd/service/shelter.py
+++ BackEnd/service/shelter.py
@@ -1,4 +1,5 @@
 from fastapi import Depends
+from schema.response import ShelterPathSchema
 from repository.shelter import ShelterRepository
 from typing import List
 
@@ -9,3 +10,8 @@ class ShelterService:
         
     async def get_near_service(self, user_location: List[float], max_distance: int = 3000) -> List[dict]:
         return await self.shelter_repository.get_near_repository(user_location, max_distance)
+    
+    def get_path_from_gps_to_shelter(self,latitude:float,longitude:float,shetler_id:str)->ShelterPathSchema:
+        
+
+        return ShelterPathSchema()
