b7fb1b9fca224310fc19a9b1e0cea799fd3faa77
tjwjdgus83 <tjwjdgus83@naver.com>
2024-03-27 09:13:15 +0900

    edit : app에 router 등록
    Change-Id: I55b04c70b9d7963f332cbd63040628226dd8382f
    
    Former-commit-id: e4b110fa8a90e18f663e337cf46b58d78f19d10e

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from database.mapdb import mapdb
 from database.mongo import mongodb
 from schema.response import PingPongSchema
-from api import shelter
+from api import shelter,path
 from resource.logo import the_end_logo
 
 
@@ -17,7 +17,9 @@ async def lifespan(app: FastAPI):
     print(the_end_logo)
     
 app = FastAPI(lifespan=lifespan)
+
 app.include_router(shelter.router)
+app.include_router(path.router)
 
 @app.get("/",status_code=200)
 async def root() -> PingPongSchema:
