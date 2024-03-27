d9a52b29a6a34c1a8a90b4b9309b47634cedc8ab
tjwjdgus83 <tjwjdgus83@naver.com>
2024-03-27 09:18:18 +0900

    edit : 클래스 이름 변경
    Change-Id: I7846f713fcedd659d3e56062b821306757a29fd7
    
    Former-commit-id: f20dad685cf5b495fb52d417b37ffe15f143bc69

--- BackEnd/database/mapdb.py
+++ BackEnd/database/mapdb.py
@@ -12,13 +12,13 @@ gps_mapping_zip_path = f"{resource_dir}/gps_mapping.zip"
 binary_map_path = f"{resource_dir}/binary_map.pkl"
 gps_mapping_path = f"{resource_dir}/gps_mapping.pkl"
 
-class mapDB:
+class MapDB:
     def __init__(self):
         self.binary_map : Optional[np.ndarray] = None
         self.gps_mapping : Optional[list] = None
 
     def load(self):
-        print("==========================mapDB=========================")
+        print("==========================MapDB=========================")
         print(mapdb_logo)
         if not os.path.isfile(binary_map_zip_path):
             raise Exception("binary_map_zip_file is not exist!!!!")
@@ -44,20 +44,20 @@ class mapDB:
             print("gps매핑을 로딩중입니다.......")
             self.gps_mapping = pickle.load(f)
             print("gps매핑의 로딩이 완료되었습니다.")
-        print("==========================mapDB=========================")
+        print("==========================MapDB=========================")
 
     def close(self):
-        print("==========================mapDB=========================")
+        print("==========================MapDB=========================")
         before_memory=psutil.virtual_memory().used/1000000
         del self.binary_map
         del self.gps_mapping
         gc.collect()
         after_memory=psutil.virtual_memory().used/1000000
-        print("로드된 mapDB이 해제되었습니다.")
+        print("로드된 MapDB이 해제되었습니다.")
         print(f"{int(before_memory-after_memory)}MB was released")
-        print("==========================mapDB=========================")
+        print("==========================MapDB=========================")
 
-mapdb=mapDB()
+mapdb=MapDB()
 
 def get_mapdb():
     return mapdb
