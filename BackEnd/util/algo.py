cf723df874ac465ae5f6b462ced60ea9ad30ce70
tjwjdgus83 <tjwjdgus83@naver.com>
2024-03-27 17:07:36 +0900

    edit : 사용자 위치로부터 특정 반경까지만의 영역 탐색으로 최적화
    Change-Id: I2fb66069ddc4012e92d200c26037259ef0cb00d0
    
    Former-commit-id: 416d7201e90880bdd676863d49325127e27fbac0

--- BackEnd/util/algo.py
+++ BackEnd/util/algo.py
@@ -10,16 +10,21 @@ INF = 1e8
 
 
 #거리가 가장 가까운 walkable_spot 반환
-def get_nearest_walkable_spot(map :list[list[int]],start:tuple) -> tuple[float,float]:
+def test_get_nearest_walkable_spot(map :np.ndarray,standard:int,start:tuple[int,int]) -> tuple[float,float]:
     print("========ready for variables==========")
-    w=len(map[0])
-    h=len(map)
-    distance=np.full_like(map,INF,dtype=np.float64)
+    print(map.shape)
+    #partial area의 영역 정하기
+    x_up=max(start[0]-standard,0)
+    x_down=min(start[0]+standard,len(map))
+    y_left=max(start[1]-standard,0)
+    y_right=min(start[1]+standard,len(map[0]))
+    h=x_down-x_up
+    w=y_right-y_left
+    distance=np.full((h,w),INF,dtype=np.float64)
     print("========ready for variables end==========")
-
     heap = []
     heapq.heappush(heap,(0,*start))
-    distance[start[0]][start[1]]=np.float64(0)
+    distance[start[0]-x_up][start[1]-y_left]=np.float64(0)
     cnt = 0
     while heap:
         dist,x,y=heapq.heappop(heap)
@@ -29,16 +34,16 @@ def get_nearest_walkable_spot(map :list[list[int]],start:tuple) -> tuple[float,f
             print(cnt)
         if(map[x][y]==1):
             return (x,y)
-        if distance[x][y] < dist:
+        if distance[x-x_up][y-y_left] < dist:
             continue
 
         for i in range(8):
             nx=x_offset[i]+x
             ny=y_offset[i]+y
-            if not (0 <= nx < h and 0 <= ny < w):
+            if not (x_up <= nx < x_down and y_left <= ny < y_right):
                 continue
             next_distance=((nx-x)**2+(ny-y)**2)**(1/2)+dist
-            if next_distance<distance[nx][ny]:
-                distance[nx][ny]=np.float64(next_distance)
+            if next_distance<distance[nx-x_up][ny-y_left]:
+                distance[nx-x_up][ny-y_left]=np.float64(next_distance)
                 heapq.heappush(heap,(next_distance,nx,ny))
     return (-1,-1)
