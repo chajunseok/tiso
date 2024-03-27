97b45fb04c41402b2a9dfc80be2597f4b425c32c
tjwjdgus83 <tjwjdgus83@naver.com>
2024-03-27 09:16:40 +0900

    edit : shleter_service 레포지토리 의존성 주입
    Change-Id: I06099722ebbae08131ac8fd5162f566e9645d6e8
    
    Former-commit-id: 9c260bb46cfe85330a06ccd254b35449805d6f89

--- BackEnd/service/shelter.py
+++ BackEnd/service/shelter.py
@@ -1,17 +1,22 @@
 from fastapi import Depends
+from database.mapdb import get_mapdb, mapDB
 from schema.response import ShelterPathSchema
 from repository.shelter import ShelterRepository
 from typing import List
 
 class ShelterService:
-    def __init__(self,shelter_repository:ShelterRepository = Depends()):
+    def __init__(self,shelter_repository:ShelterRepository = Depends(),map_db : mapDB = Depends(get_mapdb)):
         self.shelter_repository=shelter_repository
+        self.map_db=map_db
         print("ShelterService init")
         
     async def get_near_service(self, user_location: List[float], max_distance: int = 3000) -> List[dict]:
         return await self.shelter_repository.get_near_repository(user_location, max_distance)
     
-    def get_path_from_gps_to_shelter(self,latitude:float,longitude:float,shetler_id:str)->ShelterPathSchema:
-        
-
+    def get_path_from_gps_to_shelter(self,latitude:float,longitude:float,shelter_id:str)->ShelterPathSchema:
+        print(type(self.map_db.binary_map))
+        print(type(self.map_db.gps_mapping))
         return ShelterPathSchema()
+    
+    
+    
