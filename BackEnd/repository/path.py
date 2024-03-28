fd64867624d2c0cd470ea9987cac00b687033240
tjwjdgus83 <tjwjdgus83@naver.com>
2024-03-28 12:54:11 +0900

    edit : query결과 초기값 설정
    Change-Id: I29d0b418f196c5d804c3efbab842f97ea10b59db
    
    Former-commit-id: 727b14cae38438b9da9df91e533f24f62603ee5a

--- BackEnd/repository/path.py
+++ BackEnd/repository/path.py
@@ -1,4 +1,5 @@
 from fastapi import Depends
+from repository.odm import PathDocument
 from database.mongo import get_mongo_client
 
 
@@ -7,5 +8,14 @@ class PathRepository:
         self.client=mongo_client
         print("PathRepository init")
 
-    def get_path_info_by_shelter_id_and_gps(shelter_id : str , gps : tuple[float,float]):
-        pass
+    def get_path_info_by_shelter_id_and_gps(self,shelter_id : str , gps : tuple[float,float])->PathDocument:
+        print(shelter_id,gps)
+        ## mongo get
+        query_result={
+            "id":"asd",
+            "path" : [[1,2],[3,4],[4,5],[6,7]],
+            "distance" : "55.19035785"
+        }
+
+        path_document=PathDocument(data=query_result)
+        return path_document
