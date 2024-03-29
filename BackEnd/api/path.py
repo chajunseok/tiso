8b1e93ad8281111eaeefdd6037047df51d5c7df7
bohyeon0808 <bohyeon9888@gmail.com>
2024-03-29 17:50:17 +0900

    feat : Naver RestfulAPI 연결
    Change-Id: I1338d8bc50b84aeb68b11c8addf17fc24ac6b0cd
    
    Former-commit-id: 7ca195d1522ca8b3f8dacdb49ba6e029f7625609

--- BackEnd/api/path.py
+++ BackEnd/api/path.py
@@ -1,8 +1,16 @@
-from fastapi import APIRouter,Depends
+from fastapi import APIRouter, HTTPException, Depends
 from repository.odm import PathDocument
 from service.path import PathService
-from schema.response import PathRespSchema, ShelterPathSchema
+from schema.response import PathRespSchema, ShelterPathSchema, OnlyPathSchema
+from schema.request import UrlPattern
+import httpx
+import os
+from dotenv import load_dotenv
 
+load_dotenv('../.env')
+
+CLIENT_KEY = os.getenv('NAVER_CLIENT_KEY')
+SECRET_KEY = os.getenv('NAVER_SECRET_KEY')
 
 router=APIRouter(prefix="/paths",tags=["paths"])
 
@@ -19,4 +27,25 @@ async def get_path_handler(
     shelter_path_info: PathDocument=path_service.get_path_from_gps_to_shelter(latitude,longitude,shelter_id)
     #wrapper 부분
     return PathRespSchema(data=ShelterPathSchema.from_odm_to_schema(shelter_path_info))
+
+
+@router.post("/find", status_code=200)
+async def get_naver_path(url_req: UrlPattern):  #-> OnlyPathSchema:
+        url_path = url_req.url.replace(",", "%2C").replace("|", "%7C")
+        print(url_req.url)
+        
+        async with httpx.AsyncClient() as client:
+                headers = {
+                "X-NCP-APIGW-API-KEY-ID": CLIENT_KEY,
+                "X-NCP-APIGW-API-KEY": SECRET_KEY
+                }
+                try:
+                        response = await client.get(url_path, headers=headers)
+                        response.raise_for_status()
+                        # return response.text
+                        print(response.json())
+                except httpx.HTTPStatusError as exc:
+                # 서버에서 4XX나 5XX 응답을 반환했을 때 예외 처리
+                        raise HTTPException(status_code=exc.response.status_code, detail=str(exc))
+        
 
--- BackEnd/schema/response.py
+++ BackEnd/schema/response.py
@@ -23,6 +23,20 @@ class ShelterPathSchema(BaseModel):
         sheterId=orm_shelter_path.shelter_id
         return cls(path=path,distance=distance,sheterId=sheterId)
     
+    
+# Refactoring 필요
+class OnlyPathSchema(BaseModel):
+    path: list[dict[str,float]] = [
+                                    {"latitude":37.123,"longitude":127.233},
+                                    {"latitude":37.345,"longitude":127.567},
+                                    {"latitude":37.678,"longitude":127.789}
+                                ]
+    
+    @classmethod
+    def from_odm_to_schema(cls,orm_shelter_path:PathDocument):
+        path=[{"latitude":latitude,"longitude":longitude} for [longitude,latitude] in orm_shelter_path.coordinates]
+        return cls(path=path,)
+    
 class ShelterInfoSchema(BaseModel):
     shelterId: str = "65fd1f64a1c2102da599cf79"
     name: str = "구암역 대전1호선 지하역사(지하1층)"
