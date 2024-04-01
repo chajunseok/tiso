9c444dc369581f59589bf4491ba5ae0f2c45a4da
tjwjdgus83 <tjwjdgus83@naver.com>
2024-04-01 10:20:14 +0900

    edit : 변수 추가 및 자원 확인함수
    Change-Id: I5787980b829fa0a47b38a6931267b07d184bee2a
    
    Former-commit-id: 5ef29502331bb853c746a918209a7fd5b7da64a6

--- BackEnd/database/emergencydb.py
+++ BackEnd/database/emergencydb.py
@@ -6,14 +6,16 @@ from datetime import datetime
 #fastapi를 single process로 할경우만 적용가능,해당 전역변수는 현재의 app에서만 통용되기에 multiprocess일경우 redis등의 db를 활용해야한다.
 class EmergencyDB:
     def __init__(self):
-        self.is_emergency : Optional[bool] = None
+        self.status : Optional[bool] = None
         self.danger_area : Optional[dict] = None
         self.occur_time : Optional[datetime] = None
-
+        self.radius : Optional[int ] = None 
     def load(self):
         print("==========================EmergencyDB=========================")
-        self.is_emergency=False
-        self.danger_area={}
+        self.status = False
+        self.danger_area={
+        }
+        self.radius = 200
         print("비상상황 전역변수가 초기화되었습니다.")
         print("==========================EmergencyDB=========================")
 
@@ -22,9 +24,19 @@ class EmergencyDB:
         del self.is_emergency
         del self.danger_area
         del self.occur_time
+        del self.radius
         gc.collect()
         print("==========================EmergencyDB=========================")
 
+    def is_emergency(self):
+        if self.status == False:
+            return False
+        if  self.occur_time == None:
+            return False
+        diff : datetime = (datetime.now()-self.occur_time)
+        if diff.seconds > 60*30:
+            return False
+        return True
 emergencydb=EmergencyDB()
 
 def get_emergencydb():
