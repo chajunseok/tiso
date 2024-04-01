e712e79b68a1312fa79624a10827e370aa658ffc
tjwjdgus83 <tjwjdgus83@naver.com>
2024-04-01 10:21:33 +0900

    edit : mongpdb 필드 변경에 따른 쿼리 변경
    Change-Id: Ic92548eff26221e96bfd71d0afc2c96a971131f5
    
    Former-commit-id: 3c2ad7e0b4e0881be43bddd7b48d84801ae74f70

--- BackEnd/repository/path.py
+++ BackEnd/repository/path.py
@@ -1,10 +1,11 @@
 from fastapi import Depends
+from pymongo import MongoClient
 from database.pathdb import get_path_client
 from repository.odm import PathDocument
 import time
 
 class PathRepository:
-    def __init__(self,path_client=Depends(get_path_client)):
+    def __init__(self,path_client : MongoClient =Depends(get_path_client)):
         self.path_client=path_client
         print("PathRepository init")
 
@@ -14,24 +15,34 @@ class PathRepository:
         
         query = {
                 "shelter_id": shelter_id,  # 샤딩 키 조건
-                "geometry.geometries": {  # 공간 쿼리 조건
-                    "$elemMatch": {
-                        "type": "Point",
-                        "coordinates": [longitude,latitude]
-                    }
-                }
+                "start":[longitude,latitude]
             }
         start = time.time()
         query_result = collection.find_one(query)
+        print(query_result)
         end = time.time()
         print(f"path find one: {end - start:.5f} sec")
-        # print(query_result)
+    
         #아래부분은 수동매핑에서 자동매핑으로 바꿔야한다.
         if query_result != None:
-            temp_dict={
-                "shelter_id":shelter_id
-            }
-            temp_dict.update(query_result["geometry"]["geometries"][1])
-            temp_dict.update(query_result["properties"])
-            return PathDocument(data=temp_dict)
+            return PathDocument(data=query_result)
+        return PathDocument(data={})
+
+    def get_nearest_shelter_path(self, latitude: float, longitude: float)->PathDocument:
+        db = self.path_client['shelters']  # 사용할 데이터베이스 이름으로 교체
+        collection = db['path']
+        
+        #갈 수 있는 대피소 중에서 경로길이가 가장 짧은 경로 하나만 뽑아오기
+        query = [
+            { "$match":{"start": [longitude, latitude]}},
+            { "$sort":{  "distance": 1 } }
+        ]
+
+        start = time.time()
+        query_result =list(collection.aggregate(query))
+        end = time.time()
+        print(f"path find nearest one: {end - start:.5f} sec")
+        #아래부분은 수동매핑에서 자동매핑으로 바꿔야한다.
+        if len(query_result)!=0:
+            return PathDocument(data=query_result[0])
         return PathDocument(data={})
